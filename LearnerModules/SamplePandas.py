932a076a114feb030ec134b3c7742e16d0f3397d
achuri laptop <abrbalaram3@gmail.com>
2020-04-07 19:24:34 +0530

    Updated Pandas Example

--- LearnerModules/SamplePandas.py
+++ LearnerModules/SamplePandas.py
@@ -4,11 +4,15 @@ Created on Sat Apr  4 18:19:56 2020
 
 @author: HOME
 """
-import sklearn.
-import matplotlib.pyplot as plt
 import pandas as pd;
 filePath="../../DataScience/DataSets/P14-Data-Preprocessing/Data_Preprocessing/data.csv"
 df=pd.read_csv(filePath);
-df
 df.to_csv("data.csv", columns=["Age","Country"])
 #converters in DataFrames
+#GroupBY in pandas
+g=df.groupby('Country').mean()
+g=df.groupby('Age').max()
+g.describe()
+df1=df[1:4]
+df2=df[4:]
+df_combined=pd.concat([df1,df2],ignore_index=True)
