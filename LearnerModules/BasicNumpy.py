2b66a1c77d0b035beefedd462c0f5a0c926f9190
HOME <abrbalaram3@gmail.com>
2020-04-28 13:34:40 +0530

    Latest Updates

--- LearnerModules/BasicNumpy.py
+++ LearnerModules/BasicNumpy.py
@@ -1,5 +1,5 @@
 import numpy as np;
-
+import pandas as pd
 arr1 = [25, 56, 12, 85, 34, 75]
 arr2 = [42, 3, 86, 32, 856, 46]
 arr1=np.array(arr1)
@@ -10,4 +10,3 @@ arr1=arr1.astype(dtype=np.complex64)
 arr1_mat=arr1.reshape(2,3)
 arr2_mat=arr2.reshape(2,3)
 arr3=arr1_mat+arr2_mat
-print(arr3)
